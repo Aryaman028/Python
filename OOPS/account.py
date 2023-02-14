a4252230e9b8367c63584e02758d912377a757af
Aryaman Agrawal <114793920+Aryaman028@users.noreply.github.com>
2023-02-15 00:13:02 +0530

    Update account.py

--- OOPS/account.py
+++ OOPS/account.py
@@ -1,17 +1,24 @@
 class Account():
 
-    def __init__(self,name):
-        self.account_type = name
+    # def __init__(self,name):
+    #     self.account_type = name
         
-    def account_name(self):
-        return self.account_type
+    def account_name(self,name):
+        return name
 
     def add_money(self,money):
         self.deposit = money
+
     def check_balance(self):
         return self.deposit
+a = input("What type of account you want")
+Aryaman = Account()
+print("Your account is of type-->")
+print(Aryaman.account_name(a))
 
-Aryaman = Account("Savings")
-print(Aryaman.account_name)
-Aryaman.add_money(3000)
+print("how many money you want to add")
+b = int(input("enter value"))
+
+Aryaman.add_money(b)
+print("Your current balance is-->")
 print(Aryaman.check_balance())
